c5db13852c97e540e2ae25a394df2b170a6f8747
ooga518 <2401136@s.asojuku.ac.jp>
2025-07-25 09:49:18 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(function() {
 
         //入力データを取得
         let input_no = $("[name='no']").val();
-        let input_no = $("[name='score']").val();
+        let input_score = $("[name='score']").val();
         $.ajax({
             //送信方法
             type: 'POST',
